b181a184963790dfafc3a073e80da1121d37ae43
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-07-31 21:42:19 -0700

    Correct no-fix NMEA test checksum

--- tests/test_gnss_body_adapter.py
+++ tests/test_gnss_body_adapter.py
@@ -15,7 +15,7 @@ from services.locked_body_state_bridge import LockedBodyStateSnapshotBridge
 
 
 GGA_FIX = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"
-GGA_NO_FIX = "$GPGGA,123520,,,,,0,00,99.9,,,,,,*48"
+GGA_NO_FIX = "$GPGGA,123520,,,,,0,00,99.9,,,,,,*76"
 RMC_FIX = "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A"
 
 
